44e86c9c88337ed5546372f1d697f9915fabd21b
d13751200344 <d13751200344@gmail.com>
2022-04-08 02:51:24 +0800

    built-in module

--- app.js
+++ app.js
@@ -5,3 +5,8 @@ let try2 = require("./Greeting");
 
 try1.morning(myName);
 try2.night(myName);
+
+let path = require("path");
+path.extname(__filename);
+console.log(path.dirname(__filename));
+console.log(path.basename(__filename));
